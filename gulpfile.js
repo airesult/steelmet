d8faefcba0530243187c7c127e22166f45a9959e
Юрий Елисеев <ypiter@MacBook-Pro-Urij.local>
2019-04-30 18:21:18 +0300

    add tasks

--- gulpfile.js
+++ gulpfile.js
@@ -13,3 +13,40 @@ const svgSprite = require('gulp-svg-sprite');
 const svgmin = require('gulp-svgmin');
 const cheerio = require('gulp-cheerio');
 const replace = require('gulp-replace');
+
+gulp.task('sass',function(){
+    return gulp.src('scss/style.scss')
+    .pipe(plumber())
+    .pipe(sourceMaps.init())
+    .pipe(sass())
+    .pipe(autoprefixer({
+        browsers: ['last 2 version']
+    }))
+    .pipe(sourceMaps.write())
+    .pipe(gulp.dest('biuld/css'))
+    .pipe(browserSync.reload({strim: true}));
+});
+
+gulp.task('html',function(){
+    return gulp.src('*.html')
+    .pipe(gulp.dest('biuld'))
+    .pipe(browserSync.reload({strim: true}));
+});
+
+gulp.task('js',function(){
+    return gulp.src('js/**/*.js')
+    .pipe(gulp.dest('biuld/js'))
+    .pipe(browserSync.reload({strim: true}));
+});
+
+gulp.task('css',function(){
+    return gulp.src('css/**/*.css')
+    .pipe(gulp.dest('biuld/css'))
+    .pipe(browserSync.reload({strim: true}));
+});
+
+gulp.task('allimg',function(){
+    return gulp.src('img/**/*.{png,jpg}')
+    .pipe(gulp.dest('biuld/img'))
+    .pipe(browserSync.reload({strim: true}));
+});
