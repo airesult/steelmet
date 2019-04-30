2a9cc7448b5804ad98da0d43fa9689271b22af5a
Юрий Елисеев <ypiter@MacBook-Pro-Urij.local>
2019-04-30 19:21:11 +0300

    add gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -50,3 +50,85 @@ gulp.task('allimg',function(){
     .pipe(gulp.dest('biuld/img'))
     .pipe(browserSync.reload({strim: true}));
 });
+
+gulp.task('images',function(){
+    return gulp.src('build/img/**/*.{png,jpg}')
+    .pipe(imagemin([
+        imagemin.jpegtran({progressive: true}),
+        imageminJpegRecompress({
+            loops: 5,
+            min: 65,
+            max: 70,
+            quality: 'medium'
+        }),
+        imagemin.optipng({optimizationLevel: 3}),
+        pngquant({quality: '65-70', speed: 5})
+    ]))
+    .pipe(gulp.dest('biuld/img'));
+});
+
+gulp.task('svg',function(){
+    return gulp.src('build/img/**/*.svg')
+    .pipe(svgmin({
+        js2svg: {
+            pretty: true
+        }
+    }))
+    .pipe(cheerio({
+        run: function ($) {
+            $('[fill]').removeAttr('fill');
+            $('[stroke]').removeAttr('stroke');
+            $('[style]').removeAttr('style');
+        },
+        parserOptions: {xmlMode: true}
+    }))
+    .pipe(replace('&gt', '>'))
+    // build svg sprite
+    .pipe(svgSprite({
+        mode: {
+            symbol: {
+                sprite: "sprite.svg"
+            }
+        }
+    }))
+    .pipe(gulp.dest('build/img'));
+});
+
+gulp.task('serve',function(){
+    browserSync.init({
+        server: "build"
+    });
+    gulp.watch("scss/**/*.scss", ["sass"]);
+    gulp.watch("*.html", ["html"]);
+    gulp.watch("js/**/*.js", ["js"]);
+    gulp.watch("img/**/*.{png,jpg}", ["allimg"]);
+    gulp.watch("img/**/*.{svg}", ["svg"]);
+});
+
+gulp.task('copy',function(){
+    return gulp.src([
+        'img/**',
+        'js/**',
+        'css/**',
+        '*.html'
+
+    ], {
+        base: '.'
+    })
+    .pipe(gulp.dest('build'));
+});
+
+gulp.task('clean',function(){
+    return del('build');
+});
+
+gulp.task('build',function(fn){
+    run(
+        'clean',
+        'copy',
+        'sass',
+        'images',
+        'svg',
+        fn
+    );
+});
